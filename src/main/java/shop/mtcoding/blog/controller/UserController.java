fb863020c489b3c4f11e63134a2bc5ea12c0dc5e
saki8661 <saki8661@naver.com>
2023-07-27 09:04:53 +0900

    Controller 설명추가

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+    // ip주소 부여 : 10.5.9.200 -> mtcoding.com:8080 (DNL)
+    // 프로세스를 결정하는게 포트번호
+    // localhost, 127.0.0.1
+    // get요청하는 방법 : a태그-하이퍼링크, form태그-method의 get
     @GetMapping("/joinForm")
     public String joinForm() {
-        return "user/joinForm";
+        // templates
+        // .mustache
+        //
+        return "user/joinForm"; // viewResolver
     }
 
     @GetMapping("/loginForm")
